7a518a43bf5bf52eeee6cddc9c8d270fc8d2535d
Captain Kirk <captainkirk@Vincents-MacBook-Pro-2.local>
2019-04-15 11:41:49 -0400

    Updates

---
+++ precache-manifest.5624db1db0a7f3a2fde99e39ba2e0dd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "253acbcfe71c9a69b3b0",
+    "url": "/elysium/static/js/main.ff78d166.chunk.js"
+  },
+  {
+    "revision": "004ea836242e87833431",
+    "url": "/elysium/static/js/runtime~main.290463d4.js"
+  },
+  {
+    "revision": "f507c896eb9fb66322fc",
+    "url": "/elysium/static/css/2.3b3ffe22.chunk.css"
+  },
+  {
+    "revision": "f507c896eb9fb66322fc",
+    "url": "/elysium/static/js/2.15bba166.chunk.js"
+  },
+  {
+    "revision": "66d7d05646970477285532820425a862",
+    "url": "/elysium/index.html"
+  }
+];
